58448246e83a8092c7a45df1ba487f7d4bb790b5
Michael Strong <mstrong@omnitracs.com>
2020-10-12 15:18:04 -0400

    push 8

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -2,8 +2,9 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
-
+var fs = require('fs');
 var push = require('web-push');
+
 let vapidKeys = {
     publicKey: 'BOfFv9TlbYfetZ7Cd160G29UH6BUpkfweiZO-ySNtQodj-1qO59QenvY81xEnh_MepqtzK9nVrm5LQLXGnhMgR4',
     privateKey: 'W5RsU0HcLnAISVSvJ4ZrXD8jYUKqDKpGLqfnhl2Szr8'
@@ -13,13 +14,33 @@ app.get("/", function (req, res, next) {
     res.send("API found.");
 });
 
-app.post("/", function (req, res, next) {
-    console.log(req.body);
-    res.send("Post request was recieved.");
-    push.setVapidDetails('mailto:test@mike.com', vapidKeys.publicKey, vapidKeys.privateKey)
-    // put your endpoints here, the data should come from the DB
-    let sub = { "endpoint": "https://fcm.googleapis.com/fcm/send/dzxIANkdeFg:APA91bGaDfE4YCt7-PsTPK1rRHZiYfEJidvZacmyATXnHv8ixQb57N18xoUCrfP0vngq2ERzUL_FLSG1mkWHVOh5t0t9hasABgurBwc4_rxLdmvYwDtFxLdRLu3VhsHHorTd4G35pkXU", "expirationTime": null, "keys": { "p256dh": "BLh30yVxjBHuXd3mfBlSUkPImcdb-55DBUA6LwzC-BcZvv6Xmr0z6eIo1bheKuVFer6pA0JmSa6U7I_5qar7XLs", "auth": "mehr4_zunScwu11f63hrvg" } };
-    push.sendNotification(sub, 'test message');
+// loads data from file and sends to all users
+app.post("/PostNotification/", function (req, res, next) {
+    fs.readFile('mynewfile1.txt', function (err, data) {
+        //res.writeHead(200, { 'Content-Type': 'text/html' });
+        //res.write(data);
+        //return res.end();
+        //res.send(data);
+        let sub = JSON.parse(data);
+
+        push.setVapidDetails('mailto:test@mike.com', vapidKeys.publicKey, vapidKeys.privateKey)
+        push.sendNotification(sub, 'test message');
+        res.send("Post was successfully sent.");
+    });
+
+    
+});
+
+// accepts the endpoint data, amends to a file
+app.post("/SaveURL/", function (req, res, next) {
+
+    fs.appendFile('mynewfile1.txt', JSON.stringify(req.body), function (err) {
+        if (err) throw err;
+        console.log(req.body);
+        console.log('Data saved.');
+    });
+    res.send("URL was saved.");
+
 });
 
 app.listen(3000, function () {
